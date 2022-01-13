869697e19ad1991cb78b25a2aa13c4e6b36d81a3
Matt0105 <matteo0105@gmail.com>
2022-01-13 17:11:22 +0100

    footer logo completed

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,56 +2,59 @@
   <div class="footer-container">
       <div class="container">
 
-        <div class="col">
-        <h2>Dc Comics</h2>
-        <ul>
-            <li><a href="#">Characters</a></li>
-            <li><a href="#">Comics</a></li>
-            <li><a href="#">Movies</a></li>
-            <li><a href="#">Tv</a></li>
-            <li><a href="#">Games</a></li>
-            <li><a href="#">Collectables</a></li>
-            <li><a href="#">Videos</a></li>
-            <li><a href="#">Fans</a></li>
-            <li><a href="#">News</a></li>
-            <li><a href="#">Shop</a></li>
-        </ul>
+        <div class="links">
+            <div class="col">
+                <h2>Dc Comics</h2>
+                <ul>
+                    <li><a href="#">Characters</a></li>
+                    <li><a href="#">Comics</a></li>
+                    <li><a href="#">Movies</a></li>
+                    <li><a href="#">Tv</a></li>
+                    <li><a href="#">Games</a></li>
+                    <li><a href="#">Collectables</a></li>
+                    <li><a href="#">Videos</a></li>
+                    <li><a href="#">Fans</a></li>
+                    <li><a href="#">News</a></li>
+                    <li><a href="#">Shop</a></li>
+                </ul>
 
-        <h2>Shop</h2>
-        <ul>
-            <li><a href="#">Shop DC</a></li>
-            <li><a href="#">Shop DC Collections</a></li>
-            
-        </ul>
-        </div>
+                <h2>Shop</h2>
+                <ul>
+                    <li><a href="#">Shop DC</a></li>
+                    <li><a href="#">Shop DC Collections</a></li>
+                    
+                </ul>
+            </div>
 
-        <div class="col">
-            <h2>DC</h2>
-            <ul>
-                <li><a href="#">Terms Of Use</a></li>
-                <li><a href="#">Privacy policy (New)</a></li>
-                <li><a href="#">Ad Choices</a></li>
-                <li><a href="#">Advertising</a></li>
-                <li><a href="#">Jobs</a></li>
-                <li><a href="#">Subscriptions</a></li>
-                <li><a href="#">Talent Workshops</a></li>
-                <li><a href="#">CPSC Certificates</a></li>
-                <li><a href="#">Ratings</a></li>
-                <li><a href="#">Shop Help</a></li>
-                <li><a href="#">Contact Us</a></li>
-            </ul>
-        </div>
+            <div class="col">
+                <h2>DC</h2>
+                <ul>
+                    <li><a href="#">Terms Of Use</a></li>
+                    <li><a href="#">Privacy policy (New)</a></li>
+                    <li><a href="#">Ad Choices</a></li>
+                    <li><a href="#">Advertising</a></li>
+                    <li><a href="#">Jobs</a></li>
+                    <li><a href="#">Subscriptions</a></li>
+                    <li><a href="#">Talent Workshops</a></li>
+                    <li><a href="#">CPSC Certificates</a></li>
+                    <li><a href="#">Ratings</a></li>
+                    <li><a href="#">Shop Help</a></li>
+                    <li><a href="#">Contact Us</a></li>
+                </ul>
+            </div>
 
-        <div class="col">
-            <h2>Sites</h2>
-            <ul>
-                <li><a href="#">DC</a></li>
-                <li><a href="#">MAD Magazine</a></li>
-                <li><a href="#">DC Kids</a></li>
-                <li><a href="#">DC Universe</a></li>
-                <li><a href="#">DC Power Visa</a></li>
-            </ul>
+            <div class="col">
+                <h2>Sites</h2>
+                <ul>
+                    <li><a href="#">DC</a></li>
+                    <li><a href="#">MAD Magazine</a></li>
+                    <li><a href="#">DC Kids</a></li>
+                    <li><a href="#">DC Universe</a></li>
+                    <li><a href="#">DC Power Visa</a></li>
+                </ul>
+            </div>
         </div>
+        
 
         <div class="col-img">
             <img src="../assets/img/dc-logo-bg.png" alt="">
@@ -90,37 +93,53 @@ export default {
 @import "../assets/scss/partials/_variables.scss";
     .footer-container {
         width: 100%;
+        height: 480px;
         background-image: url(../assets/img/footer-bg.jpg);
-        
-    }
+
+    }   
     .container {
+        position: relative;
         display: flex;
-        // background-image: url(../assets/img/dc-logo-bg.png);
-        // background-repeat: no-repeat;
-        // background-position:right;
-        // background-size: contain;
+        justify-content: space-between;
     }
 
-    .col {
-        h2 {
-            margin-top: 2rem;
-            margin-bottom: 0.8rem;
-            margin-right: 2rem;
-            text-transform: uppercase;
-            color: white;
-        }
+    .links {
+        display: flex;
 
-        ul {
-            list-style-type: none;
-            margin-bottom: 2rem;
+        .col {
+            h2 {
+                margin-top: 2rem;
+                margin-bottom: 0.8rem;
+                margin-right: 2rem;
+                text-transform: uppercase;
+                color: white;
+            }
 
-            li a {
-                display: block;
-                text-decoration: none;
-                color: gray;
-                padding: 0.3rem;
-                padding-right: 0.5rem;
+            ul {
+                list-style-type: none;
+                margin-bottom: 2rem;
+                margin-right: 1rem;
+                li a {
+                    display: block;
+                    text-decoration: none;
+                    color: gray;
+                    padding-right: 0.5rem;
+                    padding: 0.2rem 0;
+                }
+            }
         }
+    }
+
+    .col-img {
+        height: 480px;
+        overflow: hidden;
+
+        img {
+            position: absolute;
+            top: -50px;
+            right: 0;
+            height: 120%;
+            object-position: top;
         }
     }
 
@@ -136,6 +155,11 @@ export default {
         justify-content: space-between;
     }
 
+    .footer-bottom {
+        position: relative;
+        z-index: 100;
+    }
+
     button {
             border: 1px solid $DC-blue;
             background-color: $footer-color;
@@ -147,6 +171,7 @@ export default {
     .social {
         display: flex;
         align-items: center;
+
         h2 {
             font-size: 1.3rem;
             color: $DC-blue;
